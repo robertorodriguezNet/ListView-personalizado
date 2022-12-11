f2f2560cff9a96c598527ec3b9510c63ec973a67
robertorodriguezNet <robertorodriguez@robertorodriguez.net>
2022-12-11 17:38:41 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,17 @@
+ListView personalizado
+
+Es parte de la preparación del Proyecto DAM
+Muestra un listado de productos con un adpatador* personalizado.
+
+Objetivo:
+    Muestra un listado de objetos Producto.
+    No muestra tablas relacionales, por lo que no podrá mosrar el precio más reciente.
+    Contiene un icono que permitiría añadir el producto a la lista actual.
+
+Se ha creado, por primera vez, el POJO Producto
+
+Vistas:
+    La vista principal (ativity_main.xml) carga un ListView a pantalla completa.
+
+Recuerdo: 
+* Un adaptador permite a un template mostrar el contenido de una colección.
---
+++ app/src/main/java/net/robertorodriguez/listviewpersonalizado/adaptadores/AdaptadorProductos.java
@@ -0,0 +1,67 @@
+package net.robertorodriguez.listviewpersonalizado.adaptadores;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+import net.robertorodriguez.listviewpersonalizado.R;
+import net.robertorodriguez.listviewpersonalizado.entidades.Producto;
+
+import java.util.List;
+
+public class AdaptadorProductos extends ArrayAdapter<Producto> {
+
+    Context context;                                                                  // El contexto
+    int layoutItem;                                                            // El layout del ítem
+    List<Producto> coleccionDeProductos;                                // La colección de productos
+
+    /**
+     * Constructor
+     * @param context el contexto
+     * @param layoutItem es el layout que pinta el ítem de la lista
+     * @param productos la colección de objetos
+     */
+    public AdaptadorProductos(@NonNull Context context, int layoutItem, @NonNull List<Producto> productos) {
+        super(context, layoutItem, productos);
+
+        this.context = context;
+        this.layoutItem = layoutItem;
+        this.coleccionDeProductos = productos;
+
+    }
+
+    /**
+     * Método que se lanza por cada elemento de la colección
+     * @param position posición del elemento en la lista
+     * @param convertView
+     * @param parent elemento padre del ítem (ListView)
+     * @return
+     */
+    @NonNull
+    @Override
+    public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+
+        // Se incluye el layoutTemplate en el elemento padre (ListView)
+        View view = LayoutInflater.from(context).inflate(layoutItem,parent,false);
+
+        // Obtenemos la información del elemento de la lista
+        Producto productoLeido = coleccionDeProductos.get( position );
+
+        // Elementos de la interfaz del ítem
+        TextView denominacion = (TextView) view.findViewById(R.id.tv_denominacion);
+        TextView marca = (TextView) view.findViewById(R.id.tv_marca);
+
+        // Establecemos la info del producto leído en los elementos del ítem
+        denominacion.setText( productoLeido.getDenominacion() );
+        marca.setText( Integer.toString( productoLeido.getMarca() ) );
+
+        return view;
+
+    }
+}
